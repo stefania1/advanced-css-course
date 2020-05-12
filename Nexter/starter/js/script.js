948764c3ee5e2864ed4afbfaa4fd43052b19cdb4
stefania1 <arielmonster@gmail.com>
2020-05-12 18:48:04 +0200

    Added functional sidebar menu

---
+++ Nexter/starter/js/script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidemenu").style.display = "flex";
+    document.getElementById("menu-btn").style.display = "none";
+}
+        
+function closeNav() {
+    document.getElementById("mySidemenu").style.display = "none";
+    document.getElementById("menu-btn").style.display = "flex";
+}
+
+
